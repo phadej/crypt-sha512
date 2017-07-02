caf34be806979d7f46358501837f74fecf7923e9
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-07-02 12:54:28 +0300

    cryptSHA512Raw

--- src/System/POSIX/Crypt/SHA512.hs
+++ src/System/POSIX/Crypt/SHA512.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module System.POSIX.Crypt.SHA512 (
     cryptSHA512,
-    mkSalt,
-    mkSalt',
+    cryptSHA512',
+    cryptSHA512Raw,
+    -- * Utilities
+    encode64,
+    encode64List,
     ) where
 
 import Control.Applicative (optional)
@@ -64,29 +67,11 @@ traceBSId _ = id
 --
 cryptSHA512
     :: BS.ByteString  -- ^ key
-    -> BS.ByteString  -- ^ salt
+    -> BS.ByteString  -- ^ salt: @$6$...@
     -> Maybe BS.ByteString
 cryptSHA512 key saltI = case A.parseOnly saltP saltI of
-    Left _ -> Nothing
-    Right (salt, Nothing) ->
-        let enc = implementation 5000 key salt
-        in Just $ BS.concat
-            [ header
-            , salt
-            , "$"
-            , encode64 enc
-            ]
-    Right (salt, Just rounds) ->
-        let enc = implementation rounds key salt
-        in Just $ BS.concat
-            [ header
-            , "rounds="
-            , fromString (show rounds)
-            , "$"
-            , salt
-            , "$"
-            , encode64 enc
-            ]
+    Left _               -> Nothing
+    Right (salt, rounds) -> Just (cryptSHA512Raw rounds key salt)
   where
     saltP :: A.Parser (BS.ByteString, Maybe Int)
     saltP = do
@@ -101,6 +86,40 @@ cryptSHA512 key saltI = case A.parseOnly saltP saltI of
         n <- A8.decimal
         _ <- A.word8 36
         return $ min 999999999 $ max 1000 n
+
+-- | Split salt-input variant of 'cryptSHA512'. Salt is encoded.
+cryptSHA512'
+    :: Maybe Int      -- ^ Rounds
+    -> BS.ByteString  -- ^ key
+    -> BS.ByteString  -- ^ salt, will be base64 encoded
+    -> BS.ByteString
+cryptSHA512' rounds key salt = cryptSHA512Raw rounds key (encode64 salt)
+
+-- | Raw input implementation of 'cryptSHA512'.
+cryptSHA512Raw
+    :: Maybe Int      -- ^ Rounds
+    -> BS.ByteString  -- ^ key
+    -> BS.ByteString  -- ^ salt, first 16 characters used.
+    -> BS.ByteString
+cryptSHA512Raw Nothing key salt =
+    let enc  = implementation 5000 key (BS.take 16 salt)
+    in BS.concat
+        [ header
+        , salt
+        , "$"
+        , encode64' enc
+        ]
+cryptSHA512Raw (Just rounds) key salt =
+    let enc = implementation rounds key (BS.take 16 salt)
+    in BS.concat
+        [ header
+        , "rounds="
+        , fromString (show rounds)
+        , "$"
+        , salt
+        , "$"
+        , encode64' enc
+        ]
 
 header :: BS.ByteString
 header = "$6$"
@@ -210,85 +229,74 @@ implementation roundsN key salt =
                | otherwise = y
 
 -------------------------------------------------------------------------------
--- Salt helper
--------------------------------------------------------------------------------
-
--- | Make a valid salt from random bytestring.
-mkSalt :: BS.ByteString -> BS.ByteString
-mkSalt = mkSalt' Nothing
-
--- | A variant of 'mkSalt', allowing specifying rounds.
-mkSalt'
-    :: Maybe Int      -- ^ rounds
-    -> BS.ByteString  -- ^ entropy
-    -> BS.ByteString
-mkSalt' r e = BS.concat
-    [ "$6"
-    , r'
-    , "$"
-    , encode64 e
-    , "$"
-    ]
-  where
-    r' = case r of
-        Just n -> "rounds=" `BS.append` fromString (show $ min 999999999 $ max 1000 n)
-        Nothing -> ""
-
--------------------------------------------------------------------------------
 -- Custom Base64 encoding
 -------------------------------------------------------------------------------
 
 -- | Custom base64 encoding used by crypt SHA256 scheme.
-encode64
-    :: BS.ByteString  -- should be 64 word8 long!
-    -> BS.ByteString
-encode64 bs = assemble
-    [ ( i  0, i 21, i 42 )
-    , ( i 22, i 43, i  1 )
-    , ( i 44, i  2, i 23 )
-    , ( i  3, i 24, i 45 )
-    , ( i 25, i 46, i  4 )
-    , ( i 47, i  5, i 26 )
-    , ( i  6, i 27, i 48 )
-    , ( i 28, i 49, i  7 )
-    , ( i 50, i  8, i 29 )
-    , ( i  9, i 30, i 51 )
-    , ( i 31, i 52, i 10 )
-    , ( i 53, i 11, i 32 )
-    , ( i 12, i 33, i 54 )
-    , ( i 34, i 55, i 13 )
-    , ( i 56, i 14, i 35 )
-    , ( i 15, i 36, i 57 )
-    , ( i 37, i 58, i 16 )
-    , ( i 59, i 17, i 38 )
-    , ( i 18, i 39, i 60 )
-    , ( i 40, i 61, i 19 )
-    , ( i 62, i 20, i 41 )
-    ]
-    (i 63 )
+encode64 :: BS.ByteString -> BS.ByteString
+encode64 = encode64List . BS.unpack
+
+-- | Custom base64 encoding used by crypt SHA256 scheme. See 'encode64'.
+encode64List :: [Word8] -> BS.ByteString
+encode64List = BS.pack . go
   where
-    i = BS.index bs
-
-    assemble gs l = BS.concat (map group gs) `BS.append` lastGroup l
-
-    group :: (Word8, Word8, Word8) -> BS.ByteString
-    group (b2, b1, b0) = BS.pack
+    go :: [Word8] -> [Word8]
+    go []  = []
+    go [b] =
+        [ BS.index alphabet $ fromIntegral $ b .&. 0x3f
+        , BS.index alphabet $ fromIntegral $ b `shiftR` 6
+        ]
+    go [b1, b0] =
         [ BS.index alphabet $ fromIntegral $ w .&. 0x3f
         , BS.index alphabet $ fromIntegral $ w `shiftR` 6 .&. 0x3f
         , BS.index alphabet $ fromIntegral $ w `shiftR` 12 .&. 0x3f
-        , BS.index alphabet $ fromIntegral $ w `shiftR` 18 .&. 0x3f
         ]
+      where
+        w :: Word32
+        w = fromIntegral b0 .|. fromIntegral b1 `shiftL` 8
+    go (b2:b1:b0:bs) =
+        (BS.index alphabet $ fromIntegral $ w .&. 0x3f) :
+        (BS.index alphabet $ fromIntegral $ w `shiftR` 6 .&. 0x3f) :
+        (BS.index alphabet $ fromIntegral $ w `shiftR` 12 .&. 0x3f) :
+        (BS.index alphabet $ fromIntegral $ w `shiftR` 18 .&. 0x3f) :
+        go bs
       where
         w :: Word32
         w = fromIntegral b0
             .|. fromIntegral b1 `shiftL` 8
             .|. fromIntegral b2 `shiftL` 16
 
-    lastGroup :: Word8 -> BS.ByteString
-    lastGroup b = BS.pack
-        [ BS.index alphabet $ fromIntegral $ b .&. 0x3f
-        , BS.index alphabet $ fromIntegral $ b `shiftR` 6
-        ]
-
     alphabet :: BS.ByteString
     alphabet = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+encode64'
+    :: BS.ByteString  -- should be 64 word8 long!
+    -> BS.ByteString
+encode64' bs | BS.length bs /= 64 =
+    error $ "System.POSIX.Crypt.SHA256.encode64': input should be 64 in length: " ++ show (BS.length bs)
+encode64' bs = encode64List
+    [ i  0, i 21, i 42
+    , i 22, i 43, i  1
+    , i 44, i  2, i 23
+    , i  3, i 24, i 45
+    , i 25, i 46, i  4
+    , i 47, i  5, i 26
+    , i  6, i 27, i 48
+    , i 28, i 49, i  7
+    , i 50, i  8, i 29
+    , i  9, i 30, i 51
+    , i 31, i 52, i 10
+    , i 53, i 11, i 32
+    , i 12, i 33, i 54
+    , i 34, i 55, i 13
+    , i 56, i 14, i 35
+    , i 15, i 36, i 57
+    , i 37, i 58, i 16
+    , i 59, i 17, i 38
+    , i 18, i 39, i 60
+    , i 40, i 61, i 19
+    , i 62, i 20, i 41
+    , i 63
+    ]
+  where
+    i = BS.index bs
